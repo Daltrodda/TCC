6c297fcf6a3ebf0548b70b41abd191858a364e35
Daltro Domis Araújo <daltroaraujo@tjrj.jus.br>
2025-07-11 14:23:45 -0300

    Atualização e Criação do React

---
+++ Softaware/PAREIA/frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Microservices Frontend</h1>
+      <p>This is the starting point for the React application.</p>
+    </div>
+  );
+}
+
+export default App;
